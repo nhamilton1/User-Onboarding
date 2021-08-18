48f9307f48dc1f1ccb474547f038e30cec93edb0
unknown <nickh3592@gmail.com>
2021-08-18 09:47:15 -0400

    added yup

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form'
 import Users from './components/User';
+import formSchema from './validation/formSchema';
 
 const initialFormValues = {
   username: '',
